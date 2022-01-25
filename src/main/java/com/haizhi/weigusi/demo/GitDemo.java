ee1fc69e1c24c38a16a7ae76da968e2fd54bf58e
duanbingshun <duanbingshun@haizhi.com>
2022-01-25 18:23:37 +0800

    githup demo update in dbs_1.0.0

--- src/main/java/com/haizhi/weigusi/demo/GitDemo.java
+++ src/main/java/com/haizhi/weigusi/demo/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("master01");
+        System.out.println("update in dbs_1.0.0");
     }
 }
