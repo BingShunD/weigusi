02607f230fbd50ceae8c9977591468b1cf818664
duanbingshun <duanbingshun@haizhi.com>
2022-01-25 18:20:20 +0800

    github demo update

--- src/main/java/com/haizhi/weigusi/demo/GitDemo.java
+++ src/main/java/com/haizhi/weigusi/demo/GitDemo.java
@@ -3,5 +3,6 @@ package com.haizhi.weigusi.demo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("master01");
     }
 }
