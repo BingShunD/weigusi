6a002b502f2db02c807739edb3efceec0b94c034
duanbingshun <duanbingshun@haizhi.com>
2022-01-25 18:43:36 +0800

    update

--- src/main/java/com/haizhi/weigusi/demo/GitDemo.java
+++ src/main/java/com/haizhi/weigusi/demo/GitDemo.java
@@ -4,6 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("master01");
-        System.out.println("update in dbs_1.0.0");
+        System.out.println("update in master");
     }
 }
